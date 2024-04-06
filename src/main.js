7715e74fc7ad0dec2e1f5279d9489aa150e0a260
Maicon Santana <msn-maicon@hotmail.com>
2024-04-06 17:03:28 -0300

    deploy 21.11

--- src/main.js
+++ src/main.js
@@ -1,69 +1,67 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
 
-    const heroSection = document.querySelectorAll('.hero');
-    const alturaHero = heroSection.clienteHeight;
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
 
-    window.addEventListener('scrool', function(){
+    window.addEventListener('scroll', function() {
         const posicaoAtual = window.scrollY;
 
-        if(posicaoAtual < alturaHero){
-            ocultarElementosDoHeader();
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader();
         } else {
             exibeElementosDoHeader();
         }
     })
     
-    //Seção de atrações, programação da abas
-    for (let i = 0; i < buttons.length; i++){
-        buttons[i].addEventListener('click', function(botao){
+    // Seção de atrações, programação das abas
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
-            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
-            escodeTodasAbas();
+            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`)
+            escondeTodasAbas();
             aba.classList.add('shows__list--is-active');
-            removerBotaoAtivo();
+            removeBotaoAtivo();
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
-    
-    //Seção FAQ accordeon
-    for(let i = 0; i < questions.length; i++){
-        questions[i].addEventListener('click', abreOuFechaResposta)
+
+    // Seção FAQ, accordion
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
     }
 })
 
-function ocultarElementosDoHeader(){
+function ocultaElementosDoHeader() {
     const header = document.querySelector('header');
     header.classList.add('header--is-hidden');
 }
 
-function exibeElementosDoHeader(){
+function exibeElementosDoHeader() {
     const header = document.querySelector('header');
     header.classList.remove('header--is-hidden');
 }
 
-function abreOuFechaResposta(elemento){
+function abreOuFechaResposta(e) {
     const classe = 'faq__questions__item--is-open';
-    const elementoPai =  elemento.target.parentNode;
+    const elementoPai = e.target.parentNode;
 
     elementoPai.classList.toggle(classe);
 }
 
-function removerBotaoAtivo(){
+function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
 
-    for (let i = 0; i < buttons.length; i++){
+    for (let i = 0; i < buttons.length; i++) {
         buttons[i].classList.remove('shows__tabs__button--is-active');
     }
 }
 
-function escodeTodasAbas (){
+function escondeTodasAbas() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
 
-    for (let i = 0; i < tabsContainer.length; i++){
+    for (let i = 0; i < tabsContainer.length; i++) {
         tabsContainer[i].classList.remove('shows__list--is-active');
-
     }
 }
-//parei no 30:25
